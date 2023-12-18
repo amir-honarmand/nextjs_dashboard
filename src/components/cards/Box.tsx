06e18697db921d24dd9a3def01c739128749f5d6
amir honarmand <amir.honarmand.zade@gmail.com>
2023-12-18 15:11:37 +0330

    refactor boxes

--- src/components/cards/Box.tsx
+++ src/components/cards/Box.tsx
@@ -3,23 +3,25 @@ export default function SBox({
   title,
   amount,
   desc,
-  children
+  children,
 }: {
   bgColor: string;
   title: string;
   amount: string;
   desc: string;
-  children: JSX.Element
+  children: JSX.Element;
 }) {
   return (
-    <div className={`flex flex-grow flex-col min-w-[15rem] xs:max-w-full md:max-w-[20rem] p-4 rounded-3xl ${bgColor}`}>
+    <div
+      className={`flex flex-grow flex-col max-w-full min-w-[14rem] p-4 rounded-3xl ${bgColor}`}
+    >
       <div className="flex flex-row gap-1 items-center">
         {children}
         <p className="font-bold text-sm">{title}</p>
       </div>
       <div className="flex flex-col items-center mt-5 mb-3 mx-7 gap-1">
-        <h6 className="font-bold text-xl">{amount}</h6>
-        <p className="text-center">{desc}</p>
+        <h6 className="font-bold text-lg max-w-[13rem] truncate">{amount}</h6>
+        <p className="text-center text-base max-w-[13rem] truncate">{desc}</p>
       </div>
     </div>
   );
